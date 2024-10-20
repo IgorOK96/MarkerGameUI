6d76c06ea30af2caf9dfe97c654ad162556b1e87
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-20 08:04:51 +0200

    Initial Commit

---
+++ MarkerGameUI/MarkerGameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkerGameUIApp.swift
+//  MarkerGameUI
+//
+//  Created by user246073 on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MarkerGameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
